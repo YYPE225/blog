622c6eb4bd48f949c02a74918db678a09dca1e8f
paul-emmanuel <emmanuelpaulo567@gmail.com>
2025-12-26 15:24:34 +0000

    ÉTAPE 2 — Modèles Article et Commentaire

--- blog_project/articles/models.py
+++ blog_project/articles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='articles')
+    titre = models.CharField(max_length=200)
+    contenu = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
+
+
+class Commentaire(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='commentaires')
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenu = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.auteur.username} - {self.contenu[:20]}"
